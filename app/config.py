350b96ccf5bb154ab50c8f8f25d1d77f1513afa1
Oleksandr Yurchyk <yurchyk_sasha@ukr.net>
2021-02-19 11:00:10 +0200

    make it better

--- app/config.py
+++ app/config.py
@@ -6,7 +6,7 @@ SECRET_KEY = 'd1a4771d2f3bd4f561f68f3ad19dc004855fba3db'
 
 # DB
 LOCAL_HOST = 'http://127.0.0.1:8000'
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:password@localhost/flask_db'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:olexandr3003@localhost/flask_db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_ECHO = False
 
